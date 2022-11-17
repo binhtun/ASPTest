5ca95001028a265e745a1152044edbe54eceffe9
binhtun <116136006+binhtun@users.noreply.github.com>
2022-11-17 20:05:54 +0700

    fisrt

---
+++ BaiKtTesstV3/BaiKtTesstV3/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaiKtTesstV3.Models;
+
+namespace BaiKtTesstV3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cau2()
+        {
+            return View();
+        }
+        public ActionResult KetQua(Hang h)
+        {
+            if(h.SoLuong>=100)
+            {
+                h.ThanhTien = (h.SoLuong * h.DonGia )* 0.9;
+            }
+            else
+            {
+                h.ThanhTien = h.SoLuong * h.DonGia;
+            }    
+            return View(h);
+        }
+
+    }
+}
---
+++ BaiKtTesstV3/BaiKtTesstV3/Models/Hang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaiKtTesstV3.Models
+{
+    public class Hang
+    {
+        public string MaHang { get; set; }
+        public string TenHang { get; set; }
+        public string LoaiHang { get; set; }
+        public int DonGia { get; set; }
+        public int SoLuong { get; set; }
+        public double ThanhTien { get; set; }
+    }
+}
